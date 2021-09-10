df2a2e4a9ad97d456a104070379d4adb1f94cfa3
Riegasnawan <88017958+riegasnawan@users.noreply.github.com>
2021-09-10 13:41:24 +0700

    Add files via upload

---
+++ Perpustakaan1/alert.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>fauziah</title>
+</head>
+<body>
+ 
+    <script>
+     alert('Selamat Datang di XI RPL 5');
+   </script>
+</body>
+</html>
---
+++ Perpustakaan1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-light">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="index.php">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_siswa.php">Data Siswa</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="tampil_kelas.php">Data Kelas</a>
+    </li>
+  </ul>
+</nav>
+<br>
+
+<div class="container-fluid">
+  <h3>Perpustakaan</h3>
+  <p>Selamat datang..</p>
+  <p>Terimakasih</p>
+</div>
+
+</body>
+</html>
